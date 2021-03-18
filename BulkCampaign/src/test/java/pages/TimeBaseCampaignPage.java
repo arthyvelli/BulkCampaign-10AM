bed937bf4ff705e073f856528257fdc87b8efba7
arthyvelli <48116111+arthyvelli@users.noreply.github.com>
2021-03-18 15:27:45 +0530

    Update TimeBaseCampaignPage.java
    updates

--- BulkCampaign/src/test/java/pages/TimeBaseCampaignPage.java
+++ BulkCampaign/src/test/java/pages/TimeBaseCampaignPage.java
@@ -22,79 +22,62 @@ import pages.BasePage;
 public class TimeBaseCampaignPage extends BasePage {
 	//private TestReports reports;
 	private WebDriver driver;
-    String[] Senderid=new String[5];
-    
-    
-	
+	String[] Senderid=new String[5];
+	String[] Segment=new String[5];
+
 	@FindBy(xpath="//h1[@class='font-28 font-head breadcrumbs-title']")
 	private WebElement Dashbrd;   // driver.findElement(By.xpath())
-	
+
 	@FindBy(xpath="//h1[@class='font-28 font-head breadcrumbs-title']")
 	private WebElement Campaignpage;
-	
-	
 	public TimeBaseCampaignPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-    public void getSenderId(String senderid1,String senderid2,String senderid3,String senderid4,String senderid5) {
-	//public void getSenderId(String senderid1,String senderid2) {	
-    	Senderid[0]=senderid1;
-    	Senderid[1]=senderid2;
-    	Senderid[2]=senderid3;
-    	Senderid[3]=senderid4;
-    	Senderid[4]=senderid5;
-    	
-
-    	
-    }
-	public void Bulk_TBcampaign1(String UserCount,String listname) throws IOException, InterruptedException, ParseException, java.text.ParseException{
-
+	public void getSenderId(String senderid1,String senderid2,String senderid3,String senderid4,String senderid5) {
+		//public void getSenderId(String senderid1,String senderid2) {	
+		Senderid[0]=senderid1;
+		Senderid[1]=senderid2;
+		Senderid[2]=senderid3;
+		Senderid[3]=senderid4;
+		Senderid[4]=senderid5;
+	}
+	public void getsegmentname(String segment1,String segment2,String segment3,String segment4,String segment5) {
+		Segment[0]=segment1;
+		Segment[1]=segment2;
+		Segment[2]=segment3;
+		Segment[3]=segment4;
+		Segment[4]=segment5;
+	}
+	public void Bulk_TBcampaign1(String UserCount) throws IOException, InterruptedException, ParseException, java.text.ParseException{
 		Thread.sleep(1000);
-
-		
 		int userscount=Integer.parseInt(UserCount);
-
 		BasePage.waitForElement(driver,Dashbrd,50);
-
 		driver.navigate().to("http://dash.vinmail.io/campaign");
-
 		BasePage.waitForElement(driver,Campaignpage,50);
-
 		//Take campaign duplicate
 		CampaignDuplicatePage campdup=new CampaignDuplicatePage(driver);
-
 		//int Campcount=excel.getnum(0, i, 2);
 		System.out.println("No.of campaigns ="+userscount);
-
 		//String myTime = "16:00";
 		/*SimpleDateFormat df = new SimpleDateFormat("HH:mm");
 
 		Date d = df.parse(myTime); 
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(d);				 
-*/
+		 */
 		//this.getSenderId(senderid1, senderid2, senderid3);
 		for(int j=0;j<userscount;j++) 
 		{
-			    
-			
-				campdup.DuplicateEdit(Senderid[j],listname);
-
+			campdup.DuplicateEdit(Senderid[j],Segment[j]);
 			/*	cal.add(Calendar.MINUTE,5);
 				String newTime = df.format(cal.getTime());*/
-
-				//System.out.println("Campaign"+j+" = "+myTime);
-
-				campdup.timeBaseSchedule();
-				//campdup.Scheduleconfirm();
-			
-			}
-		   
-				
-			//excel.closeWrokbbok();
-			System.out.println("Executed Succesfully");
-		    
+			//System.out.println("Campaign"+j+" = "+myTime);
+			campdup.timeBaseSchedule();
+			//campdup.Scheduleconfirm();
+		}
+		//excel.closeWrokbbok();
+		System.out.println("Executed Succesfully");
 		LogoutPage LogOut= new LogoutPage(driver);
 		LogOut.peformLogout();
 
